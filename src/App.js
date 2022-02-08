13a1f5d1951404838e7a3d9f04b8d2ebd63b9253
Colin Sand <Ottermij2@gmail.com>
2022-02-08 17:48:14 -0600

    Adds axios query to API, and search/input bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
-import ApiKey from "./ApiKey";
 
 function App() {
+  // stores the response data and the location sent to the api in state
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  // after looking at stack overflow, looks like api keys will still be shown even with a gitignore file used
-  // adding the key back directly into the App.js component
+  // brings in the API url to be queried
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b1fe57d7c51185c339fd5482541c08ef
   `;
 
+  const searchLocation = (event) => {
+    if (event.key === `Enter`) {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
+
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Please enter a location"
+          type="text"
+        ></input>
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>Omaha</p>
           </div>
           <div className="temp">
             <h1>60 degrees f</h1>
