c5432858d2e44edce521397a7eddb35ed4e1d651
Colin Sand <Ottermij2@gmail.com>
2022-02-08 16:39:47 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  // const url =
+  //   "https://api.openweathermap.org/data/2.5/weather?q=omaha&appid=b1fe57d7c51185c339fd5482541c08ef";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Testing</h1>
     </div>
   );
 }
