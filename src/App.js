64849c5d965ef8a00ea7b8e172691c496cff4280
Colin Sand <Ottermij2@gmail.com>
2022-02-08 17:34:21 -0600

    adds start of api call code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import axios from "axios";
 import ApiKey from "./ApiKey";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  // after looking at stack overflow, looks like api keys will still be shown even with a gitignore file used
+  // adding the key back directly into the App.js component
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b1fe57d7c51185c339fd5482541c08ef
+  `;
+
   return (
     <div className="app">
       <div className="container">
