8a5647e5d8f68f128660aae5da1770786257f681
Colin Sand <Ottermij2@gmail.com>
2022-02-08 17:08:01 -0600

    Hides API key with gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
+import ApiKey from "./ApiKey";
 
 function App() {
-  // const url =
-  //   "https://api.openweathermap.org/data/2.5/weather?q=omaha&appid=b1fe57d7c51185c339fd5482541c08ef";
   return (
-    <div>
-      <h1>Testing</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60 degrees f</h1>
+          </div>
+          <div className="description">Clouds</div>
+        </div>
+        <div className="bottom">
+          <div className="feels like:">
+            <p>65 degrees f</p>{" "}
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 mph</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
