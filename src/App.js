bbf6cb354b0200160e450489556674451b7b3fba
Colin Sand <Ottermij2@gmail.com>
2022-02-08 18:15:07 -0600

    Adds api data to display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-function App() {
+export default function App() {
   // stores the response data and the location sent to the api in state
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
   // brings in the API url to be queried
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b1fe57d7c51185c339fd5482541c08ef
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=b1fe57d7c51185c339fd5482541c08ef
   `;
 
   const searchLocation = (event) => {
@@ -16,11 +16,14 @@ function App() {
         setData(response.data);
         console.log(response.data);
       });
+      setLocation(``);
     }
   };
 
   return (
     <div className="app">
+      {" "}
+      Test
       <div className="search">
         <input
           value={location}
@@ -33,27 +36,28 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Omaha</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>60 degrees f</h1>
+            {data.main ? <h1>{data.main.temp} °F</h1> : null}
           </div>
-          <div className="description">Clouds</div>
+          <div className="description">{data.weather[0].main}</div>
         </div>
-        <div className="bottom">
-          <div className="feels like:">
-            <p>65 degrees f</p>{" "}
+
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels like:">
+              {data.main ? <p>Feels Like: {data.main.feels_like} °F</p> : null}
+            </div>
+            <div className="humidity">
+              {data.main ? <p>Humidity: {data.main.humidity} %</p> : null}
+            </div>
+            <div className="wind">
+              {data.main ? <p>Wind Speed: {data.wind.speed} Mph</p> : null}
+            </div>
           </div>
-          <div className="humidity">
-            <p>20%</p>
-          </div>
-          <div className="wind">
-            <p>12 mph</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
 }
-
-export default App;
